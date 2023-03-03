d609415b99e23a4b4cbb7bdb1f5d579f105bbe45
Faboya Basil korede <106436258+Faboya-korede@users.noreply.github.com>
2023-03-03 16:29:56 +0100

    Update env.sh

--- env.sh
+++ env.sh
@@ -1,11 +1,11 @@
 #! /usr/bin/bash
 
-sed -i~ '/^DB_HOST=/s/=.*/="127.0.0.1"/' /var/www/html/laravel-realworld-example-app/.env
+sed -i~ '/^DB_HOST=/s/=.*/="DB-HOST"/' /var/www/html/laravel-realworld-example-app/.env
 sed -i~ '/^DB_PORT=/s/=.*/="3306"/'  /var/www/html/laravel-realworld-example-app/.env
-sed -i~ '/^DB_DATABASE=/s/=.*/="laravel_realworld"/'  /var/www/html/laravel-realworld-example-app/.env
-sed -i~ '/^DB_USERNAME=/s/=.*/="korede"/'  /var/www/html/laravel-realworld-example-app/.env
-sed -i~ '/^DB_PASSWORD=/s/=.*/="password"/'  /var/www/html/laravel-realworld-example-app/.env
-sed -i~ '/^DB_URL=/s/=.*/="67.207.86.131"/'  /var/www/html/laravel-realworld-example-app/.env
-sed -i~ '/^APP_URL=/s/=.*/="www.korede.me"/'  /var/www/html/laravel-realworld-example-app/.env
+sed -i~ '/^DB_DATABASE=/s/=.*/="Db-NAME"/'  /var/www/html/laravel-realworld-example-app/.env
+sed -i~ '/^DB_USERNAME=/s/=.*/="DB-USER"/'  /var/www/html/laravel-realworld-example-app/.env
+sed -i~ '/^DB_PASSWORD=/s/=.*/="DB-PASSWORD"/'  /vAr/www/html/laravel-realworld-example-app/.env
+sed -i~ '/^DB_URL=/s/=.*/="DB-URL"/'  /var/www/html/laravel-realworld-example-app/.env
+sed -i~ '/^APP_URL=/s/=.*/="APP-URL"/'  /var/www/html/laravel-realworld-example-app/.env
 sed -i~ '/^DB_CONNECTION=/s/=.*/="mysql"/'  /var/www/html/laravel-realworld-example-app/.env
 
